ce6b8f713a15af14724d221e40b6178b3bf83f9f
jsp5a08 <jsp5a08@scs.local>
2024-04-17 12:24:14 +0800

    v0.55
    make scenes
    menu
    gp
    results

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,8 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class SongAssign : MonoBehaviour
+public class GameManager : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
